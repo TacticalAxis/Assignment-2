7413d61a17b7586e9049e9a2621ce20af0295937
bombard21 <wangxiaofeng153@gmail.com>
2022-09-05 14:39:07 +1200

    code

---
+++ src/main/java/comp611/assignment2/subdivisions/BruteForceApproach.java
@@ -0,0 +1,85 @@
+package comp611.assignment2.subdivisions;
+
+import java.util.Scanner;
+
+public class BruteForceApproach {
+    int [][] area;
+    int x,y;
+
+
+    public BruteForceApproach(int x, int y) {
+        this.x = x;
+        this.y = y;
+        area = new int[y][x];
+    }
+
+
+    //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    //print 2d array and fill it with
+//    public void printArea() {
+//        for (int i = 0; i < y; i++) {
+//            for (int j = 0; j < x; j++) {
+//                System.out.print(area[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//
+//    public void createSubDivisions() {
+//
+//
+//    }
+//
+//    private void fillArea(int x1,int y1,int x2,int y2,char fill) {
+//        for (int i = x1; i < x2; i++) {
+//            for (int j = y1; j < y2; j++) {
+//                area[i][j] = fill;
+//            }
+//        }
+//    }
+//
+//
+//
+//
+//    public static void main(String[] args) {
+//        BruteForceApproach bruteForceApproach = new BruteForceApproach(6,3);
+//        bruteForceApproach.createSubDivisions();
+//        bruteForceApproach.printArea();
+//    }
+
+
+
+
+
+
+
+}
