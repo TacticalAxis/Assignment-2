72c4921fcc8a1af5448e0ed84ebd35b6a8021c89
bombard21 <wangxiaofeng153@gmail.com>
2022-09-12 12:10:52 +1200

    Update BruteForceApproach.java

--- src/main/java/comp611/assignment2/subdivisions/approach/BruteForceApproach.java
+++ src/main/java/comp611/assignment2/subdivisions/approach/BruteForceApproach.java
@@ -4,21 +4,14 @@ import comp611.assignment2.subdivisions.land.Area;
 import comp611.assignment2.subdivisions.land.Land;
 import comp611.assignment2.subdivisions.land.Subdivision;
 
-public class BruteForceApproach extends Approach<Double> {
+public class BruteForceApproach extends Approach<Double, Subdivision> {
 
     private int subdivisions = 0;
 
-    // Calling the constructor of the super class, Approach.
     public BruteForceApproach(Land land) {
         super(land, "Brute Force Approach");
     }
 
-    /**
-     * The function `solve()` is called by the `main()` function in the `Main` class. It starts the timer, calls the
-     * `findSub()` function, stops the timer, prints the number of subdivisions, and returns 0.0
-     *
-     * @return The return type is Double, but the method returns 0.0.
-     */
     @Override
     public Double solve() {
         startTimer();
@@ -28,9 +21,7 @@ public class BruteForceApproach extends Approach<Double> {
         return 0.0;
     }
 
-
     @Override
-    // Finding the best subdivision for the given area.
     public Subdivision findBest(Area area) {
         if(area != null) {
             double best = 0.0d;
@@ -49,12 +40,6 @@ public class BruteForceApproach extends Approach<Double> {
         return null;
     }
 
-    /**
-     * If the area can be subdivided, then for each possible subdivision, subdivide the area, find the subdivisions of the
-     * two new areas, and then unsubsidize the area
-     *
-     * @param area The area to be subdivided
-     */
     private void findSub(Area area) {
         if (area != null) {
             if (area.canSubdivide()) {
@@ -62,7 +47,7 @@ public class BruteForceApproach extends Approach<Double> {
                     area.subdivide(subdivision);
                     findSub(area.getArea1());
                     findSub(area.getArea2());
-                    area.unSubdivide();
+//                    area.unSubdivide();
                     subdivisions++;
                 }
 
@@ -77,9 +62,4 @@ public class BruteForceApproach extends Approach<Double> {
             }
         }
     }
-
-
-
-
 }
-
