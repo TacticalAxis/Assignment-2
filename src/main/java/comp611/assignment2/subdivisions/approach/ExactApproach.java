fe8242beec92e591ac004603fa73010a87cfbcb4
bombard21 <wangxiaofeng153@gmail.com>
2022-09-17 17:50:00 +1200

    Code

--- src/main/java/comp611/assignment2/subdivisions/approach/ExactApproach.java
+++ src/main/java/comp611/assignment2/subdivisions/approach/ExactApproach.java
@@ -4,40 +4,37 @@ import comp611.assignment2.subdivisions.land.Area;
 import comp611.assignment2.subdivisions.land.Land;
 import comp611.assignment2.subdivisions.land.Subdivision;
 
+import java.util.HashMap;
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class ExactApproach extends Approach {
 
-    private static ExactApproachLayoutSet permutations;
-
+    private int currentLandValue;
+    private int subdivisions;
+    private HashMap<Subdivision, Integer> subMap = new HashMap<>();
     public ExactApproach(Land area) {
         super(area, "Exact Approach");
-        this.permutations = new ExactApproachLayoutSet(area);
     }
 
     @Override
     public Result solve() {
-        // start timer
         startTimer();
 
-        // start recursive search
-        getAllIterations(getLand().getArea());
+        //recursive search
+        findSubDivisions(getLand().getArea());
 
-        // stop timer
         stopTimer();
 
-        // get solution
-        Area area = permutations.findBest();
-        if(area != null) {
-            // solution found
+        //call the method to find the best value
+        int area = Solution();
+        if(area != 0) {
             getLand().setArea(area);
-
-            // return result
-            return new Result(this, area.getLand().getValue(), getSubdivisions());
+            return new Result(this, area, subdivisions);
         }
 
-        // no solution found
+
+        //no solution found
         return null;
     }
 
@@ -46,123 +43,86 @@ public class ExactApproach extends Approach {
         return 0;
     }
 
-    //
-    //get all possible itterations
-    public void getAllIterations(Area area){
-        if(area == null){
-            return;
-        }
-
-        //check if the area is divisible
-        if(area.canSubdivide()){
-            return;
-        }
-
-        if(area.getRoot().isFullySubdivided()){
-            area.getRoot().getSubdivisions();
-            return;
-        }
-
-//         This is the recursive function that is used to find all possible solutions.
-        for(Subdivision sub : area.getSubdivisions()){
+    //create subdivide method
+    private int findSubDivisions(Area area){
+        //get all possible subDivisions
+        List<Subdivision> subDivisions = area.getSubdivisions();
+        //iterate through all possible subDivisions
+        for(Subdivision sub : subDivisions){
+            //subdivide the area
             area.subdivide(sub);
+            //increment the number of subdivisions
             incrementSubdivisions();
-            permutations.add(area.getRoot().copy());
-            getAllIterations(area.getArea1());
-            getAllIterations(area.getArea2());
-            area.unSubdivide();
+            //check if the value of the land is greater than the current value
+            if(area.getLand().getValue() > currentLandValue){
+                //if it is, set the current value to the new value
+                currentLandValue = (int) area.getLand().getValue();
+                //add the subdivision to the hashmap
+                subMap.put(sub, currentLandValue);
+                //call the method again
+                findSubDivisions(area.getArea1());
+                findSubDivisions(area.getArea2());
+            } else {
+                //if the value is not greater than the current value, unSubdivide the area
+                area.unSubdivide();
+            }
         }
-
-
-        //find the single most optimal solution
-        Area best = permutations.findBest();
-        if(best != null) {
-            getLand().setArea(best);
-        }
-
+        return currentLandValue;
 
     }
 
-    public int getPossibleLayouts() {
-        return permutations.size();
+    //create a method to find the best value
+    private int Solution(){
+        //create a variable to store the best value
+        int bestValue = 0;
+        //iterate through the hashmap
+        for(Subdivision sub : subMap.keySet()){
+            //check if the value is greater than the best value
+            if(subMap.get(sub) > bestValue){
+                //if it is, set the best value to the new value
+                bestValue = subMap.get(sub);
+            }
+        }
+        return bestValue;
     }
 
-    private static class ExactApproachLayoutSet {
-        private final List<Area> values;
-        private final Land land;
 
-        public ExactApproachLayoutSet(Land land) {
-            this.land = land;
-            this.values = new java.util.ArrayList<>();
-        }
-
-        public int size() {
-            return values.size();
-        }
-
-        private void add(Area area) {
-            if(area == null) {
-                return;
-            }
-
-            Area toAdd = area.copy();
-            for(Area a : values) {
-                if(a.compare(toAdd)) {
-                    return;
-                }
-            }
-
-            values.add(toAdd);
-        }
-
-//        private Area getSolution() {
-//            if (values.isEmpty()) {
-//                return null;
-//            }
-//
-//            Area best = values.get(0);
-//            for (Area area : values) {
-//                if (land.getValue(area) > land.getValue(best)) {
-//                    best = area;
+//    private Subdivision getBestSub() {
+//        //iterate through the hashmap
+//        Iterator<Subdivision> iterator = subMap.keySet().iterator();
+//        Subdivision bestSub = null;
+//        while(iterator.hasNext()){
+//            //get the next subdivision
+//            Subdivision sub = iterator.next();
+//            //if the bestSub is null, set it to the first subdivision
+//            if(bestSub == null){
+//                bestSub = sub;
+//            } else {
+//                //if the value of the subdivision is greater than the value of the bestSub, set the bestSub to the subdivision
+//                if(subMap.get(sub) > subMap.get(bestSub)){
+//                    bestSub = sub;
 //                }
 //            }
-//
-//            return best;
 //        }
-
-        public Area findBest() {
-            if (values.isEmpty()) {
-                return null;
-            }
-
-            Area best = values.get(0);
-            for (Area area : values) {
-                if (land.getValue(area) > land.getValue(best)) {
-                    best = area;
-                }
-            }
-
-            values.clear();
-            values.add(best);
-            return best;
-        }
-    }
+//        //return the bestSub
+//        return bestSub;
+//    }
 
     public static void main(String[] args) {
-        ExactApproach exactApproach = new ExactApproach(new Land(9, 9,50,20,1000));
-        Result result = exactApproach.solve();
-        if(result != null) {
-            System.out.println("Solution found: " + result.getValue());
-            System.out.println("Subdivisions: " + exactApproach.getSubdivisions());
-            System.out.println("Time: " + exactApproach.getTime() + "ms");
-            System.out.println("Possible layouts: " + exactApproach.getPossibleLayouts());
-            System.out.println("Solution: " + exactApproach.getLand());
+        ExactApproach exactApproach = new ExactApproach(new Land(6, 3, 50, 20,1000));
+        Result solution = exactApproach.solve();
+        if(solution != null) {
+//            System.out.println("Greedy Solution Found: " + solution.getValue());
+//            System.out.println("This took " + exactApproach.getTime() + "s");
+//            System.out.println("Subdivisions Found: " + exactApproach.getSubdivisions());
+//            System.out.println("Solution:\n" + exactApproach.getLand());
+            System.out.println("Solution for best value: " + exactApproach.getBestValue());
         }
 
 //        JFrame frame = new JFrame("Land GUI");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 //
-//        frame.getContentPane().add(new LandGUI(exactApproach.getLand(), frame));
+//        frame.getContentPane().add(new LandGUI(exactApproach, frame));
 //        frame.pack();
 //
 //        frame.setLocationRelativeTo(null);
@@ -170,45 +130,4 @@ public class ExactApproach extends Approach {
     }
 
 
-    //depth first search of the width and the height of the land
-    public class depthFirstSearch {
-        private int width;
-        private int height;
-        private int[][] land;
-        private Approach approach;
-
-
-        public depthFirstSearch(int width, int height){
-            this.width = width;
-            this.height = height;
-        }
-
-        public void search(Area area){
-            //search the width
-            for(int i = 0; i < width; i++){
-                //search the height
-                for(int j = 0; j < height; j++){
-                    //check if the area is divisible
-                    if(area.canSubdivide()){
-                        return;
-                    }
-
-                    if(area.getRoot().isFullySubdivided()){
-                        area.getRoot().getSubdivisions();
-                        return;
-                    }
-
-                    // This is the recursive function that is used to find all possible solutions.
-                    for(Subdivision sub : area.getSubdivisions()){
-                        area.subdivide(sub);
-                        approach.incrementSubdivisions();
-                        permutations.add(area.getRoot().copy());
-                        getAllIterations(area.getArea1());
-                        getAllIterations(area.getArea2());
-                        area.unSubdivide();
-                    }
-                }
-            }
-        }
-    }
 }
