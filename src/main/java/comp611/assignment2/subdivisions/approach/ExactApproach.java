a9d5afc3b40f1c2022a4eda442915668946f7f3c
bombard21 <wangxiaofeng153@gmail.com>
2022-09-17 21:32:59 +1200

    Update ExactApproach.java

--- src/main/java/comp611/assignment2/subdivisions/approach/ExactApproach.java
+++ src/main/java/comp611/assignment2/subdivisions/approach/ExactApproach.java
@@ -1,9 +1,12 @@
 package comp611.assignment2.subdivisions.approach;
 
+import comp611.assignment2.subdivisions.gui.LandGUI;
 import comp611.assignment2.subdivisions.land.Area;
 import comp611.assignment2.subdivisions.land.Land;
 import comp611.assignment2.subdivisions.land.Subdivision;
 
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -36,97 +39,77 @@ public class ExactApproach extends Approach {
             return new Result(this, area.getLand().getValue(), getSubdivisions());
         }
 
-
-
-
         //no solution found
         return null;
     }
 
     @Override
     public double getBestValue() {
-        return 0;
+        return currentLandValue;
     }
 
-    //create subdivide method
-    private Area findSubDivisions(Area area){
-        //get all possible subDivisions
-        List<Subdivision> subDivisions = area.getSubdivisions();
-        //iterate through all possible subDivisions
-        for(Subdivision sub : subDivisions){
+    //recursive method to find all the subdivisions
+    private void findSubDivisions(Area area) {
+        //get all the subdivisions
+        //loop through all the subdivisions
+        for(Subdivision sub : area.getPossibleSubdivisions().keySet()) {
             //subdivide the area
             area.subdivide(sub);
-            //increment the number of subdivisions
-            incrementSubdivisions();
-            //check if the value of the land is greater than the current value
-            if(area.getLand().getValue() > currentLandValue){
+            //increment the subdivisions
+            subdivisions++;
+            //add the subdivision to the hashmap
+            subMap.put(sub, subdivisions);
+            //check if the value is greater than the current value
+            if(area.getLand().getValue() > currentLandValue) {
                 //if it is, set the current value to the new value
                 currentLandValue = (int) area.getLand().getValue();
-                //add the subdivision to the hashmap
-                subMap.put(sub, currentLandValue);
-                //call the method again
+                //call the recursive method again
                 findSubDivisions(area.getArea1());
                 findSubDivisions(area.getArea2());
             } else {
-                //if the value is not greater than the current value, unSubdivide the area
+                //if it is not, unSubdivide the area
                 area.unSubdivide();
+                //remove the subdivision from the hashmap
+                subMap.remove(sub);
+                //decrement the subdivisions
+                subdivisions--;
             }
         }
-        return area;
-
     }
 
-    //create a method to find the best value
-    private Area getSolution(){
-        //returns the value stored in the hashmap
-        if(subMap.containsValue(currentLandValue)){
-            return getLand().getArea();
+    //method to get the solution
+    private Area getSolution() {
+        //get the area
+        Area area = getLand().getArea();
+        //loop through the hashmap
+        for(Subdivision sub : subMap.keySet()) {
+            //subdivide the area
+            area.subdivide(sub);
         }
-        return null;
-
+        //return the area
+        return area;
     }
 
-
-//    private Subdivision getBestSub() {
-//        //iterate through the hashmap
-//        Iterator<Subdivision> iterator = subMap.keySet().iterator();
-//        Subdivision bestSub = null;
-//        while(iterator.hasNext()){
-//            //get the next subdivision
-//            Subdivision sub = iterator.next();
-//            //if the bestSub is null, set it to the first subdivision
-//            if(bestSub == null){
-//                bestSub = sub;
-//            } else {
-//                //if the value of the subdivision is greater than the value of the bestSub, set the bestSub to the subdivision
-//                if(subMap.get(sub) > subMap.get(bestSub)){
-//                    bestSub = sub;
-//                }
-//            }
-//        }
-//        //return the bestSub
-//        return bestSub;
-//    }
 
     public static void main(String[] args) {
-        ExactApproach exactApproach = new ExactApproach(new Land(6, 3, 50, 20,1000));
+        ExactApproach exactApproach = new ExactApproach(new Land(6, 10, 70, 20,1000));
         Result solution = exactApproach.solve();
         if(solution != null) {
-//            System.out.println("Greedy Solution Found: " + solution.getValue());
-//            System.out.println("This took " + exactApproach.getTime() + "s");
-//            System.out.println("Subdivisions Found: " + exactApproach.getSubdivisions());
-//            System.out.println("Solution:\n" + exactApproach.getLand());
-            System.out.println("Solution for best value: " + exactApproach.getBestValue());
+            System.out.println("Greedy Solution Found: " + solution.getValue());
+            System.out.println("This took " + exactApproach.getTime() + "s");
+            System.out.println("Subdivisions Found: " + exactApproach.getSubdivisions());
+            System.out.println("Solution:\n" + exactApproach.getLand());
+
         }
 
-//        JFrame frame = new JFrame("Land GUI");
-//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-//
-//        frame.getContentPane().add(new LandGUI(exactApproach, frame));
-//        frame.pack();
-//
-//        frame.setLocationRelativeTo(null);
-//        frame.setVisible(true);
+        JFrame frame = new JFrame("Land GUI");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(new LandGUI(exactApproach, frame));
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
 
