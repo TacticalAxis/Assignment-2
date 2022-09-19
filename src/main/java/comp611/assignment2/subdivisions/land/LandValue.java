d413b3dbfe5df9500da59df7cedbfa5d25b99222
bombard21 <wangxiaofeng153@gmail.com>
2022-09-19 13:11:18 +1200

    Update LandValue.java

--- src/main/java/comp611/assignment2/subdivisions/land/LandValue.java
+++ src/main/java/comp611/assignment2/subdivisions/land/LandValue.java
@@ -58,4 +58,33 @@ public class LandValue {
     public double getValue(int width, int height) {
         return valueTable[height - 1][width - 1];
     }
+
+
+    public void landValueTable(){
+        //2d array of land values
+        int[][] landValue = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+                            {1},
+                            {2},
+                            {3},
+                            {4},
+                            {5},
+                            {6},
+                            {7},
+                            {8},
+                            {9},
+                            {10},
+                            {11},
+                            {12},
+                            {13},
+                            {14},
+                            {15},
+                            {18},
+                            {19},
+                            {16},
+                            {17},
+                            {20}};
+    }
+
+    public static void main(String[] args) {
+    }
 }
