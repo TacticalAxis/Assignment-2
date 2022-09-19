8e7e0b8f4afe48788734664edf2326072c16448f
bombard21 <wangxiaofeng153@gmail.com>
2022-09-19 23:42:52 +1200

    Update Maths.java

--- src/main/java/comp611/assignment2/subdivisions/Maths.java
+++ src/main/java/comp611/assignment2/subdivisions/Maths.java
@@ -4,6 +4,8 @@ public class Maths {
     public static double[][] getValueTable(int height, int width, double minValue, double maxValue) {
         double[][] valueTable = new double[height][width];
 
+        int random = (int) (Math.random() * 100);
+
         //for loop that fills the outer edge of numbers
         //from 0,0 to 0,height and 0,0 to width,0
         for (int i = 1; i < height; i++) {
@@ -13,7 +15,10 @@ public class Maths {
         }
         for (int i = 1; i < width; i++) {
             //values would be powers of 2
+
             valueTable[0][i] = Math.pow(2, i);
+            //round to 2 decimal places
+
 //            valueTable[0][i] = i;
         }
 
@@ -21,9 +26,17 @@ public class Maths {
         //using the formula ( x + y )- 1
         for (int i = 1; i < height; i++) {
             for (int j = 1; j < width; j++) {
-                //using the formula of adding the axis values together and - 1
-                valueTable[i][j] = (valueTable[i][0] + valueTable[0][j]) - 1;
-//                valueTable[i][j] = valueTable[i - 1][j] + valueTable[i][j - 1] - valueTable[i - 1][j - 1] - 1;
+                //using the formula of adding the axis values together and adding 10% to it or subtracting 10%
+                if(random % 2 == 0) {
+                    //round to 2 decimal places
+                    valueTable[i][j] = Math.round((valueTable[i - 1][j] + valueTable[i][j - 1]) * 1.1 * 100.0) / 100.0;
+//                    valueTable[i][j] = (valueTable[i - 1][j] + valueTable[i][j - 1]) * 1.1;
+                } else {
+                    //round to 2dp
+                    valueTable[i][j] = Math.round((valueTable[i - 1][j] + valueTable[i][j - 1]) * 0.9 * 100.0) / 100.0;
+//                    valueTable[i][j] = (valueTable[i - 1][j] + valueTable[i][j - 1]) * 0.9;
+                }
+//                valueTable[i][j] = (valueTable[i][0] + valueTable[0][j]);
             }
         }
 
